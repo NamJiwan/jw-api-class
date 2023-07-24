a7eb790b1a52ac275054ab6395ded0290cfed91c
NamJiwan <skawldhks@naver.com>
2023-07-24 12:58:42 +0900

    end

--- js/map.js
+++ js/map.js
@@ -4,11 +4,14 @@ const url = new URL(window.location.href);
 const params = new URLSearchParams(url.search);
 city = params.get("city");
 city = city ? city : "대구광역시";
+
+
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };  
+
 
 // 지도를 생성합니다    
 var map = new kakao.maps.Map(mapContainer, mapOption); 
